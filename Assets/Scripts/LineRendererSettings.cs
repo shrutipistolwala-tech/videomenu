aebf942dbd03773bfddb34953d91108b1600b99f
Shruti Pistolwala <shruti.pistolwala@tum.de>
2022-12-10 15:17:12 +0100

    Working Prototype

--- Assets/Scripts/LineRendererSettings.cs
+++ Assets/Scripts/LineRendererSettings.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class LineRendererSettings : MonoBehaviour
 {
+    public GameObject panel;
+    public Image img;
+
+    public Button btn;
+
     [SerializeField] LineRenderer rend;
     Vector3[] points;
     // Start is called before the first frame update
     void Start()
     {
+        img = panel.GetComponent<Image>();
+
         rend = gameObject.GetComponent<LineRenderer>();
 
         points = new Vector3[2];
@@ -23,26 +32,62 @@ public class LineRendererSettings : MonoBehaviour
 
         public LayerMask layerMask;
 
-        public void AlignLineRenderer(LineRenderer renderer)
+        public bool AlignLineRenderer(LineRenderer rend)
         {
+            
             Ray ray;
             ray = new Ray(transform.position, transform.forward);
             RaycastHit hit;
+            bool hitBtn = false;
 
             if (Physics.Raycast(ray, out hit, layerMask))
             {
                 points[1] = transform.forward + new Vector3(0, 0, hit.distance);
+                rend.startColor = Color.red;
+                rend.endColor = Color.red;
+                btn = hit.collider.gameObject.GetComponent<Button>();
+                hitBtn = true;
             }
             else
             {
                 points[1] = transform.forward + new Vector3(0, 0, 20);
+                rend.startColor = Color.green;
+                rend.endColor = Color.green;
+                hitBtn = false;
             }
             rend.SetPositions(points);
+            rend.material.color = rend.startColor;
+            return hitBtn;
+        }
+
+        public void ColorChangeOnClick()
+        {
+            if (btn !=null)
+            {
+                if (btn.name == "red_btn")
+                {
+                    img.color = Color.red;
+                }
+                 else if(btn.name == "blue_btn")
+                 {
+                    img.color = Color.blue;
+                 }
+                 else if(btn.name == "green_btn")
+                 {
+                    img.color = Color.green;
+                    Debug.Log("Green");
+                 }
+            }
+           
         }
 
     // Update is called once per frame
     void Update()
     {
-        AlignLineRenderer(rend);
+    if (AlignLineRenderer(rend) && Input.GetAxis("Submit")>0)
+    {
+
+        btn.onClick.Invoke();
     }
+}
 }
